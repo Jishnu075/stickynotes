199a8e2bd606b1354e1a8c7b448fc407dd05d31b
Jishnu Biju <jishnubiju075@gmail.com>
2025-01-27 17:50:30 +0530

    delete popup and func added

--- lib/note_detail_screen.dart
+++ lib/note_detail_screen.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:stickynotes/add_note_screen.dart';
+import 'package:stickynotes/db_service.dart';
 import 'package:stickynotes/home_screen.dart';
+import 'package:stickynotes/note_provider.dart';
 import 'package:stickynotes/notes_model.dart';
 
 class NoteDetailScreen extends StatelessWidget {
@@ -28,13 +32,51 @@ class NoteDetailScreen extends StatelessWidget {
     dueTEC.text = (_getDaysUntil(note.due) + 1).toString();
     return Scaffold(
         appBar: AppBar(
-            title: const Text('detail'),
-            backgroundColor: note.colorHEX.isEmpty
-                ? Color(int.parse(NoteColors.gold.hexCode))
-                : Color(int.parse(note.colorHEX))),
+          title: const Text('detail'),
+          backgroundColor: note.colorHEX.isEmpty
+              ? Color(int.parse(NoteColors.postItYellow.hexCode))
+              : Color(int.parse(note.colorHEX)),
+          actions: [
+            IconButton(
+              onPressed: () {
+                showCupertinoModalPopup(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                          title: Text('Remove'),
+                          content: Text('Are you sure?'),
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.maybePop(context),
+                                child: const Text('cancel')),
+                            TextButton(
+                                style: TextButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromARGB(12, 244, 67, 54),
+                                    foregroundColor: Colors.redAccent),
+                                onPressed: () async {
+                                  await DBService().deleteNote(note);
+                                  if (context.mounted) {
+                                    Provider.of<NoteProvider>(context,
+                                            listen: false)
+                                        .fetchNotes();
+                                    Navigator.pop(context);
+                                    Navigator.maybePop(context);
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        const SnackBar(
+                                            content: Text('note deleted')));
+                                  }
+                                },
+                                child: const Text('delete'))
+                          ],
+                        ));
+              },
+              icon: Icon(Icons.delete),
+            )
+          ],
+        ),
         body: Container(
           color: note.colorHEX.isEmpty
-              ? Color(int.parse(NoteColors.gold.hexCode))
+              ? Color(int.parse(NoteColors.postItYellow.hexCode))
               : Color(int.parse(note.colorHEX)),
           child: SafeArea(
             child: Column(
