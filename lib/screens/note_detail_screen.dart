2a4c576e63eca38ffbe5c21f4d2c8eaf2b0059c7
Jishnu Biju <jishnubiju075@gmail.com>
2025-01-27 19:01:18 +0530

    added a temp ui for detail screen

--- lib/screens/note_detail_screen.dart
+++ lib/screens/note_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:stickynotes/screens/add_note_screen.dart';
 import 'package:stickynotes/db_service.dart';
@@ -79,42 +80,143 @@ class NoteDetailScreen extends StatelessWidget {
               ? Color(int.parse(NoteColors.postItYellow.hexCode))
               : Color(int.parse(note.colorHEX)),
           child: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  flex: 7,
-                  child: Column(children: [
-                    TextField(
-                      controller: titleTEC,
-                    ),
-                    TextField(
-                      controller: descTEC,
-                    ),
-                    TextField(
-                        controller: dueTEC,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(prefixText: 'days :')),
-                  ]),
-                ),
-                Flexible(
-                    flex: 3,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    flex: 7,
+                    child: Column(
+                        // mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          CloseButton(),
-                          OutlinedButton(
-                            onPressed: () {},
-                            child: Text("update"),
+                          MinimalTextField(
+                            controller: titleTEC,
+                            prefixText: 'title: ',
                           ),
-                          Container(
-                              width: 20,
-                              height: 20,
-                              color: getCardColor(colorHEX: note.colorHEX))
-                        ]))
-              ],
+                          const SizedBox(height: 10),
+                          MinimalTextField(
+                            controller: descTEC,
+                            maxLines: null,
+                            prefixText: 'description: ',
+                          ),
+                          const SizedBox(height: 10),
+                          MinimalTextField(
+                            prefixText: 'days: ',
+                            controller: dueTEC,
+                            keyboardType: TextInputType.number,
+                          ),
+                        ]),
+                  ),
+                  Flexible(
+                      flex: 3,
+                      child: Row(children: [
+                        const Spacer(),
+                        OutlinedButton(
+                          onPressed: () {},
+                          child: Text("update"),
+                        ),
+                        Container(
+                            width: 20,
+                            height: 20,
+                            color: getCardColor(colorHEX: note.colorHEX))
+                      ]))
+                ],
+              ),
             ),
           ),
         ));
+  }
+}
+
+class MinimalTextField extends StatelessWidget {
+  final String? hintText;
+  final String? labelText;
+  final String? prefixText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final int? maxLines;
+  final TextInputAction? textInputAction;
+  final FocusNode? focusNode;
+
+  const MinimalTextField({
+    super.key,
+    this.hintText,
+    this.labelText,
+    this.prefixText,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.onChanged,
+    this.validator,
+    this.maxLines = 1,
+    this.textInputAction,
+    this.focusNode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      onChanged: onChanged,
+      validator: validator,
+      maxLines: maxLines,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      style: const TextStyle(
+        fontSize: 16,
+        color: Colors.black87,
+      ),
+      decoration: InputDecoration(
+        prefixText: prefixText,
+        hintText: hintText,
+        labelText: labelText,
+        hintStyle: const TextStyle(
+          color: Colors.black38,
+          fontSize: 16,
+        ),
+        filled: true,
+        fillColor: Colors.grey[100],
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(
+            color: Colors.black26,
+            width: 1,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: Colors.red.shade300,
+            width: 1,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: Colors.red.shade300,
+            width: 1,
+          ),
+        ),
+      ),
+    );
   }
 }
