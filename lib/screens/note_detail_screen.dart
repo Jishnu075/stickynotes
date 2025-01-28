a48d95dc6cb68bd594af40794f1535024d51cd0e
Jishnu Biju <jishnubiju075@gmail.com>
2025-01-28 11:49:51 +0530

    refactor

--- lib/screens/note_detail_screen.dart
+++ lib/screens/note_detail_screen.dart
@@ -7,6 +7,7 @@ import 'package:stickynotes/db_service.dart';
 import 'package:stickynotes/screens/home_screen.dart';
 import 'package:stickynotes/note_provider.dart';
 import 'package:stickynotes/notes_model.dart';
+import 'package:stickynotes/widgets/minimal_text_field.dart';
 
 class NoteDetailScreen extends StatelessWidget {
   NoteDetailScreen({
@@ -145,96 +146,5 @@ class NoteDetailScreen extends StatelessWidget {
             ),
           ),
         ));
-  }
-}
-
-class MinimalTextField extends StatelessWidget {
-  final String? hintText;
-  final String? labelText;
-  final String? prefixText;
-  final TextEditingController? controller;
-  final bool obscureText;
-  final TextInputType keyboardType;
-  final void Function(String)? onChanged;
-  final String? Function(String?)? validator;
-  final int? maxLines;
-  final TextInputAction? textInputAction;
-  final FocusNode? focusNode;
-
-  const MinimalTextField({
-    super.key,
-    this.hintText,
-    this.labelText,
-    this.prefixText,
-    this.controller,
-    this.obscureText = false,
-    this.keyboardType = TextInputType.text,
-    this.onChanged,
-    this.validator,
-    this.maxLines = 1,
-    this.textInputAction,
-    this.focusNode,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: obscureText,
-      keyboardType: keyboardType,
-      onChanged: onChanged,
-      validator: validator,
-      maxLines: maxLines,
-      textInputAction: textInputAction,
-      focusNode: focusNode,
-      style: const TextStyle(
-        fontSize: 16,
-        color: Colors.black87,
-      ),
-      decoration: InputDecoration(
-        prefixText: prefixText,
-        hintText: hintText,
-        labelText: labelText,
-        hintStyle: const TextStyle(
-          color: Colors.black38,
-          fontSize: 16,
-        ),
-        filled: true,
-        fillColor: Colors.grey[100],
-        contentPadding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 12,
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide.none,
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: BorderSide.none,
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(5),
-          borderSide: const BorderSide(
-            color: Colors.black26,
-            width: 1,
-          ),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(
-            color: Colors.red.shade300,
-            width: 1,
-          ),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8),
-          borderSide: BorderSide(
-            color: Colors.red.shade300,
-            width: 1,
-          ),
-        ),
-      ),
-    );
   }
 }
---
+++ lib/widgets/minimal_text_field.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MinimalTextField extends StatelessWidget {
+  final String? hintText;
+  final String? labelText;
+  final String? prefixText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final void Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final int? maxLines;
+  final TextInputAction? textInputAction;
+  final FocusNode? focusNode;
+
+  const MinimalTextField({
+    super.key,
+    this.hintText,
+    this.labelText,
+    this.prefixText,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.onChanged,
+    this.validator,
+    this.maxLines = 1,
+    this.textInputAction,
+    this.focusNode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: obscureText,
+      keyboardType: keyboardType,
+      onChanged: onChanged,
+      validator: validator,
+      maxLines: maxLines,
+      textInputAction: textInputAction,
+      focusNode: focusNode,
+      style: const TextStyle(
+        fontSize: 16,
+        color: Colors.black87,
+      ),
+      decoration: InputDecoration(
+        prefixText: prefixText,
+        hintText: hintText,
+        labelText: labelText,
+        hintStyle: const TextStyle(
+          color: Colors.black38,
+          fontSize: 16,
+        ),
+        filled: true,
+        fillColor: Colors.grey[100],
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 12,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(
+            color: Colors.black26,
+            width: 1,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: Colors.red.shade300,
+            width: 1,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(
+            color: Colors.red.shade300,
+            width: 1,
+          ),
+        ),
+      ),
+    );
+  }
+}
